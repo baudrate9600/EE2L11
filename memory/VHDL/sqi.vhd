698be6788e254705c03b3aed5206ef6f5c0824bd
Olasoji <olasojimakinwa@gmail.com>
2020-11-26 10:39:58 +0000

    -

--- memory/VHDL/sqi.vhd
+++ memory/VHDL/sqi.vhd
@@ -9,8 +9,8 @@ entity sqi is
         en       : out std_logic;
    	RW	 : in std_logic;
    	address  : in std_logic_vector (14 downto 0); 
-   	data_in  : in std_logic_vector(7 downto 0); 
-	data_out : in std_logic_vector(7 downto 0);    
+   	data_in  : in std_logic_vector(3 downto 0); 
+	data_out : in std_logic_vector(3 downto 0);    
         chip_data_in  : in std_logic_vector(3 downto 0);
         chip_data_out : out std_logic_vector(3 downto 0);
    	done     : out std_logic;
