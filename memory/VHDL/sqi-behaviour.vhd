d4d57716fb7f1d03a893ab1cdb3ebfd217b30229
Olasoji <olasojimakinwa@gmail.com>
2020-11-26 10:34:34 +0000

    -

---
+++ memory/VHDL/sqi-behaviour.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+architecture behaviour of sqi is
+	type sqi_statae is (START, SENDING, READING, DONE);
+begin
+	process (clk) 
+	begin
+
+	end process;
+end behaviour;
+
--- memory/VHDL/sqi.vhd
+++ memory/VHDL/sqi.vhd
@@ -1,12 +1,16 @@
+--SQI Module 
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity sqi is
    port(reset    : out std_logic;
-        clk      : out std_logic;
+        clk      : in std_logic;
         cs       : out std_logic;
         en       : out std_logic;
+   	RW	 : in std_logic;
+   	address  : in std_logic 16 downto 0; 
         data_in  : in std_logic_vector(3 downto 0);
         data_out : out std_logic_vector(3 downto 0));
+   	done     : out std_logic;
 end sqi;
 
