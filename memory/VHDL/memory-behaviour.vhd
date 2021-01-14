005ac3b8d139ad1634b345f1d53330d7ffeb6a9b
Geert Custers <geert.aj.custers@gmail.com>
2021-01-14 09:01:12 +0100

    fix address constant

--- memory/VHDL/memory-behaviour.vhd
+++ memory/VHDL/memory-behaviour.vhd
@@ -495,7 +495,7 @@ FSM:	process(state, ce, sqi_finished, reset, sqi_data_in, counter, x, y, rw, i_c
 				if (grid = '0') then
 					new_sqi_address <= std_logic_vector(resize(((row) + (column * 32 * 26)), sqi_address'length));
 				else
-					new_sqi_address <= std_logic_vector(resize(((row) + (column * 32 * 26)) + 1048576, sqi_address'length));
+					new_sqi_address <= std_logic_vector(resize(((row) + (column * 32 * 26)) + 65536, sqi_address'length));
 				end if;
 				sqi_data_out(6 downto 1) <= calc_buf_in;
 				sqi_data_out(0) <= '0';
